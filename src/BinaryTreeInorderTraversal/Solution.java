bedba1b643ae5b6a82a91a43b487bb7a76690e0e
Ran Chen <chenr09@gmail.com>
2014-09-11 10:05:49 -0400

    AC

---
+++ src/BinaryTreeInorderTraversal/Solution.java
@@ -0,0 +1,32 @@
+package BinaryTreeInorderTraversal;
+
+import Tools.ListTools;
+import Tools.TreeNode;
+import Tools.TreeTools;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * User: Ran Chen <ranc@cs.cmu.edu>
+ * Date: 9/11/14
+ * Time: 9:56 AM
+ */
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> l = new LinkedList<Integer>();
+        visit(root, l);
+        return l;
+    }
+
+    public void visit(TreeNode root, List<Integer> l) {
+        if (root == null) return;
+        visit(root.left, l);
+        l.add(root.val);
+        visit(root.right, l);
+    }
+
+    public static void main(String[] args) {
+        new ListTools().PrintList(new Solution().inorderTraversal(new TreeTools().getTree2()));
+    }
+}
