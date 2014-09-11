e1da929cbfe254fb05c2218b174d92a999825749
Ran Chen <chenr09@gmail.com>
2014-09-11 14:43:34 -0400

    AC

--- src/IntegerToRoman/Solution.java
+++ src/IntegerToRoman/Solution.java
@@ -8,60 +8,25 @@ package IntegerToRoman;
  */
 public class Solution {
     public String intToRoman(int num) {
-        String result = "";
-        int[] number = {10, 50, 100, 500, 1000};
-        int[] list = new int[number.length];
-        String string = "XLCDM";
-        for (int i = number.length - 1; i >= 0; i--) {
-            list[i] = num / number[i];
-            num = num % number[i];
-        }
-        for (int i = number.length - 1; i >= 0; i--) {
-            result += repeat(string.charAt(i), list[i]);
-        }
-        switch (num) {
-            case 0:
-                break;
-            case 1:
-                result += "I";
-                break;
-            case 2:
-                result += "II";
-                break;
-            case 3:
-                result += "III";
-                break;
-            case 4:
-                result += "IV";
-                break;
-            case 5:
-                result += "V";
-                break;
-            case 6:
-                result += "VI";
-                break;
-            case 7:
-                result += "VII";
-                break;
-            case 8:
-                result += "VIII";
-                break;
-            case 9:
-                result += "IX";
-                break;
-        }
-        return result;
-    }
+        int[] weights = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] tokens = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            for (int i = 0; i < weights.length; i++) {
+                int count = num / weights[i];
+                num = num % weights[i];
+                for (int j = 0; j < count; j++) {
+                    sb.append(tokens[i]);
+                }
 
-    public String repeat(char c, int t) {
-        String s = "";
-        for (int i = 0; i < t; i++) {
-            s += c;
+            }
         }
-        return s;
+
+        return sb.toString();
+
     }
 
     static public void main(String[] args) {
-        System.out.println(new Solution().intToRoman(40));
+        System.out.println(new Solution().intToRoman(109));
     }
 }
