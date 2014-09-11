295cf7074d38fbe4a25cd5b7fd7417ac19809ad8
Ran Chen <chenr09@gmail.com>
2014-09-11 13:45:02 -0400

    AC

--- src/BestTimeToBuyAndSellStock/Solution.java
+++ src/BestTimeToBuyAndSellStock/Solution.java
@@ -8,7 +8,20 @@ package BestTimeToBuyAndSellStock;
 public class Solution {
     public int maxProfit(int[] prices) {
         if (prices.length == 0) return 0;
-        return 0;
+        int max = Integer.MIN_VALUE;
+        int profit = 0;
+        for (int i = prices.length - 1; i >= 0; i--) {
+            if (prices[i] > max) {
+                max = prices[i];
+            } else {
+                int delta = max - prices[i];
+                if (delta > profit) {
+                    profit = delta;
+                }
+            }
+
+        }
+        return profit;
     }
 
     public static void main(String[] args) {
