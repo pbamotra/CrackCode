d9990cfaeaf0c5069e1a2ec093fb57d4e188085d
Ran Chen <chenr09@gmail.com>
2014-09-15 21:49:21 -0400

    AC

---
+++ src/BinaryTreeLevelOrderTraversal/Solution.java
@@ -0,0 +1,53 @@
+package BinaryTreeLevelOrderTraversal;
+
+import Tools.ListTools;
+import Tools.TreeNode;
+import Tools.TreeTools;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * User: Ran Chen <ranc@cs.cmu.edu>
+ * Date: 9/15/14
+ * Time: 9:25 PM
+ */
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new LinkedList<List<Integer>>();
+        if (root == null) return result ;
+        List<Integer> list = new LinkedList<Integer>();
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        int i = 0;
+        int j = 1;
+        queue.add(root);
+        while (i <= j && i < queue.size()) {
+            list = new LinkedList<Integer>();
+            int count = 0;
+            for (int k = i; k < j; k++) {
+                TreeNode n = queue.get(k);
+                list.add(n.val);
+                if (n.left != null) {
+                    count++;
+                    queue.add(n.left);
+                }
+                if (n.right != null) {
+                    count++;
+                    queue.add(n.right);
+                }
+            }
+            result.add(list);
+            i = j;
+            j = j + count;
+        }
+
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ListTools.PrintListList(new Solution().levelOrder(TreeTools.getTree1()));
+    }
+}
